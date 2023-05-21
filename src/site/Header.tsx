cae21d4d99eeade1e55fba60d025245eb84e0442
Ilya Esenko <ilya.esenko@gmail.com>
2023-05-21 17:41:31 +0300

    First mircotask

---
+++ src/site/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+type HeaderType={
+    titleForHeader:string
+}
+export const Header = (props:HeaderType) => {
+    return (
+        <>
+            <div>{props.titleForHeader}</div>
+        </>
+    );
+};
+
