966f51f91c4e6923e06545f9a8d82638fde371f1
Ilya Esenko <ilya.esenko@gmail.com>
2023-06-15 17:35:42 +0300

    update filter component

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,14 @@ import {CarsComponent} from "./Components/CarsComponent";
 import Button from "./Components/button";
 import FilterComponent from "./Components/filterComponent";
 
+export type MoneyType = {
+    banknots: string
+    value: number
+    number: string
+}
+export type FilterType = 'all' | 'dollar' | 'ruble'
+
+
 function App() {
 
     const topCars = [
@@ -24,9 +32,9 @@ function App() {
     }
 
 
-    let[a, setA] = useState(1)
+    let [a, setA] = useState(1)
 
-    const onClickHandler = () =>{
+    const onClickHandler = () => {
         setA(++a)
 
     }
@@ -34,6 +42,37 @@ function App() {
     const onClickHandlerZero = () => {
         setA(0)
     }
+
+    const [money, setMoney] = useState([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
+
+
+    const [filter, setFilter] = useState<FilterType>('all')
+
+    let currentMoney = money
+
+    if (filter === 'ruble') {
+        currentMoney = currentMoney.filter((filteredMoney) => filteredMoney.banknots === 'RUBLS')
+    }
+
+    if (filter === 'dollar') {
+        currentMoney = currentMoney.filter((filteredMoney) => filteredMoney.banknots === 'Dollars')
+    }
+
+
+    const onClickFilterHandler = (nameButton: FilterType) => {
+        setFilter(nameButton)
+    }
+
+
     return (
 
 
@@ -51,10 +90,17 @@ function App() {
             <button onClick={onClickHandlerZero}>0</button>
 
             -----------------------------------------------
-            <FilterComponent/>
+            <FilterComponent currentMoney={currentMoney} foo={onClickFilterHandler}/>
+
+
         </div>
     )
         ;
 }
 
+
+/*
+В коде в разных местах допущена одна и та же ошибка.
+Какое слово должно быть написано вместо ошибочного?
+*/
 export default App;
--- src/Components/filterComponent.tsx
+++ src/Components/filterComponent.tsx
@@ -1,63 +1,44 @@
 import React, {useState} from 'react';
-
-type FilterType = 'all' | 'dollar' | 'ruble'
-
-const Filter = () => {
-
-    const [money, setMoney] = useState([
-        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
-        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
-        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
-        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
-        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
-    ])
+import {FilterType, MoneyType} from "../App";
 
 
-    const [filter, setFilter] = useState<FilterType>('all')
+type FilterComponentType = {
+    currentMoney: Array<MoneyType>
+    foo:(nameButton: FilterType)=>void
+}
 
-    let currentMoney = money;
-    if (filter === 'ruble') {
-        currentMoney = currentMoney.filter((filteredMoney) => filteredMoney.banknots === 'RUBLS')
-    }
-    if (filter === 'dollar') {
-        currentMoney = currentMoney.filter((filteredMoney) => filteredMoney.banknots === 'Dollars')
-    }
+const Filter = (props:FilterComponentType) => {
 
 
-    const onClickFilterHandler = (nameButton: FilterType) => {
-        setFilter(nameButton)
-    }
-    return (
 
-        <>
-            <ul>
-                {
-                    currentMoney.map((objFromMoneyArr, index: number) => {
-                            return (
-                                <li key={index}>
-                                    <span>{objFromMoneyArr.banknots}</span>
-                                    <span>{objFromMoneyArr.value}</span>
-                                    <span>{objFromMoneyArr.number}</span>
-                                </li>
-                            )
-                        }
-                    )
-                }
+return (
 
-            </ul>
+    <>
+        <ul >
+            {props.currentMoney.map((objFromMoneyArr, index: number) => {
+                        return (
+                            <li key={index}>
+                                <span>{objFromMoneyArr.banknots}</span>
+                                <span>{objFromMoneyArr.value}</span>
+                                <span>{objFromMoneyArr.number}</span>
+                            </li>
+                        )
+                    }
+                )
+            }
 
-            <div>
-                <button onClick={() => onClickFilterHandler('all')}>all</button>
-                <button onClick={() => onClickFilterHandler('ruble')}>ruble</button>
-                <button onClick={() => onClickFilterHandler('dollar')}>dollar</button>
-            </div>
-        </>
+        </ul>
+
+        <div style={{marginLeft: '35px'}}>
+            <button onClick={() => props.foo('all')}>all</button>
+            <button onClick={() => props.foo('ruble')}>ruble</button>
+            <button onClick={() => props.foo('dollar')}>dollar</button>
+        </div>
+    </>
 
 
-    );
-};
+);
+}
+;
 
 export default Filter;
